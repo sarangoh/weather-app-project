f3484fe851a8b581d61c3a8042df51096ef7d2dc
sarangoh <79591318+sarangoh@users.noreply.github.com>
2021-03-19 08:28:45 -0700

    Build 12 hour forecast

--- src/script.js
+++ src/script.js
@@ -42,6 +42,15 @@ let now = new Date();
 let nowTime = document.querySelector("#time");
 nowTime.innerHTML = getTime(now);
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) hours = `0${hours}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) minutes = `0${minutes}`;
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -68,7 +77,29 @@ function displayTemperature(response) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 4; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class ="col-3">
+  <ul>
+    <h3>
+    ${formatHours(forecast.dt * 1000)}
+    </h3>
+    <img src= "http://openweathermap.org/img/wn/${
+      forecast.weather[0].icon
+    }@2x.png"/>
+ <strong>H: ${Math.round(forecast.main.temp_max)}°</strong> | L: ${Math.round(
+      forecast.main.temp_min
+    )}°
+  <li>Clear</li>
+</ul>
+</div>
+`;
+  }
 }
 
 function search(city) {
@@ -76,8 +107,8 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}$units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
